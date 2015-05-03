76f2c3852d4dc70d085f8d76e15e78b3a51cd0f6
Lewis Wright <lewis@vivait.co.uk>
2015-05-03 08:02:53 +0100

    Removed doctrine cache

--- src/Vivait/Chronofy/Chronofy.php
+++ src/Vivait/Chronofy/Chronofy.php
@@ -2,10 +2,7 @@
 
 namespace Vivait\Chronofy;
 
-use Doctrine\Common\Cache\Cache;
-use Doctrine\Common\Cache\FilesystemCache;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Vivait\Chronofy\Exception\CacheException;
 use Vivait\Chronofy\Exception\FileException;
 use Vivait\Chronofy\Exception\HttpException;
 use Vivait\Chronofy\Exception\TokenExpiredException;
@@ -42,18 +39,12 @@ class Chronofy
     protected $options;
 
     /**
-     * @var Cache
-     */
-    private $cache;
-
-    /**
      * @param null $clientId
      * @param null $clientSecret
      * @param array $options
      * @param HttpAdapter $http
-     * @param Cache $cache
      */
-    public function __construct($token, array $options = [], HttpAdapter $http = null, Cache $cache = null)
+    public function __construct($token, array $options = [], HttpAdapter $http = null)
     {
         $this->optionsResolver = new OptionsResolver();
         $this->setOptions($options);
@@ -62,12 +53,6 @@ class Chronofy
             $this->http = $http;
         } else {
             $this->http = new GuzzleAdapter();
-        }
-
-        if ($cache) {
-            $this->cache = $cache;
-        } else {
-            $this->cache = new FilesystemCache(sys_get_temp_dir());
         }
 
         $this->token = $token;
@@ -85,7 +70,6 @@ class Chronofy
     {
         $resolver->setDefaults([
             'token_refresh' => true,
-            'cache_key' => 'token',
 //            'oauth_url' => 'https://api.cronofy.com/',
             'url' => 'https://api.cronofy.com/v1/',
         ]);
@@ -125,23 +109,12 @@ class Chronofy
      */
     protected function performRequest($method, $resource, array $request, array $headers, $returnType = HttpAdapter::RETURN_TYPE_JSON)
     {
-//        if ($this->cache->contains($this->options['cache_key'])) {
-//            $accessToken = $this->cache->fetch($this->options['cache_key']);
-//        } else {
-//            $accessToken = $this->authorize();
-//            $this->cache->save($this->options['cache_key'], $accessToken);
-//        }
-
         try {
             $headers['Authorization'] = 'Bearer '. $this->token;
 
             return $this->http->$method($resource, $request, $headers, $returnType);
 
         } catch (UnauthorizedException $e) {
-
-            if(!$this->cache->delete($this->options['cache_key'])){
-                throw new CacheException('Could not delete the key in the cache. Do you have permission?');
-            }
 
             if ($e instanceof TokenExpiredException || $e instanceof TokenInvalidException) {
                 if ($this->options['token_refresh']) {
